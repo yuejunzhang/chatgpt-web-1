dc0c6c73a7be013e83c5615af8c4f34427bd3b77
yuejunzhang <58799137+yuejunzhang@users.noreply.github.com>
2023-07-22 15:13:34 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
+      avatar: 'https://raw.githubusercontent.com/yuejunzhang/chatgpt-web-1/main/src/assets/avatar.jpg',
+      name: 'zhangyuejun',
+      description: 'Star on <a href="https://github.com/yuejunzhang/chatgpt-web-1" class="text-blue-500" target="_blank" >GitHub</a>',
     },
   }
 }
